cfa3e617026bd0609887d58b02ce3a2918022292
hungnguyen50497 <nguyenhung50497@gmail.com>
2022-11-08 13:50:02 +0700

    new

---
+++ main.js
@@ -0,0 +1,37 @@
+// let profile = {
+
+//     fname: 'Code',
+  
+  
+//     lname: 'Gym',
+  
+  
+//     bday: new Date('1979-01-02')
+  
+  
+//   }
+  
+  
+//   let fname = profile.fname;
+  
+  
+//   let lname = profile.lname;
+  
+//   console.log(fname, lname); 
+let profile = {
+
+    fname: 'Code',
+  
+  
+    lname: 'Gym',
+  
+  
+    bday: new Date('1979-01-02')
+  
+  
+  }
+  
+  
+  let {fname, lname} = profile;
+  
+  console.log(fname, lname); 
